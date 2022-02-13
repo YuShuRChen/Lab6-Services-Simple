fa9c502f89adf8f1b4744d5844ed0f930a2644c5
YuShuRChen <a0936582936@gmail.com>
2022-02-13 15:12:34 -0800

    Completed

---
+++ app/src/main/java/com/example/lab6_services_simple/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.lab6_services_simple;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onStartServiceClicked(View view) {
+        Intent intent = new Intent(MainActivity.this, DemoService.class);
+        startService(intent);
+    }
+
+    public void onStopServiceClicked(View view) {
+        Intent intent = new Intent(MainActivity.this, DemoService.class);
+        stopService(intent);
+    }
+}
